c7a0aa66a6629d9ed2660ca24e623b15c5298d81
Eduardo <ibredu@gmail.com>
2018-06-27 14:25:19 -0500

    Added ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  users = [
+    {nick: 'Eduardo', subnick: 'Mi mensaje personal', status: 'online', age: 28, email: 'eduardo@platzi.com', friend: true},
+    {nick: 'Yuliana', subnick: 'Mi mensaje personal', status: 'busy', age: 25, email: 'yuliana@platzi.com', friend: true},
+    {nick: 'Freddy', subnick: 'Mi mensaje personal', status: 'away', age: 28, email: 'freddy@platzi.com', friend: false}
+  ];
   constructor() { }
 
   ngOnInit() {
